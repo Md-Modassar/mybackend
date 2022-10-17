68218deb7619eb913b2e818ed1702c970250a5d4
Md-Modassar <mdmodassar622@gmail.com>
2022-10-17 22:11:54 +0530

    my assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -31,6 +31,70 @@ router.get('/students/:studentName', function(req, res){
     res.send('The full name is ' + myParams.studentName )
 })
 
+  router.get('/move',function(req,res){
+    const array=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send('that is array:'+array)
+  })
+  router.get('/moves/:index',function(req,res){
+    
+    const arr=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let index=req.params.index
+    if(index<arr.length)
+    {
+    res.send( arr[index])
+    }
+    else{
+        res.send("that is out of array")
+    }
+  })
+  router.get('/films',function(req,res){
+    const array=
+    [ {
+     id: 1,
+     name: 'The Shining'
+    }, {
+     id: 2,
+     name: 'Incendies'
+    }, {
+     id: 3,
+     name: 'Rang de Basanti'
+    }, {
+     id: 4,
+     name: 'Finding Nemo'
+    }]
+    res.send(array)
+    
+  })
+  router.get('/films/:filmid',function(req,res){
+    const array=
+    [ {
+     id: 1,
+     name: 'The Shining'
+    }, {
+     id: 2,
+     name: 'Incendies'
+    }, {
+     id: 3,
+     name: 'Rang de Basanti'
+    }, {
+     id: 4,
+     name: 'Finding Nemo'
+    }]
+    let filmid=req.params.filmid
+    for(let i=0; i<array.length; i++)
+    {
+        const object=array[i]
+          if(object.id==filmid)
+          {
+            res.send(array[filmid-1])
+          }
+    }
+
+      res.send("that is not exsist")
+        
+
+  })
+  
 // Example 2 for path params
 router.get('/student-details/:name', function(req, res){
     let requestParams = req.params
