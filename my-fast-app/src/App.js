16b7049415a23393f7cbfb3f500c96eabff4dd1d
Md-Modassar <mdmodassar622@gmail.com>
2023-01-05 20:27:46 +0530

    don

--- my-fast-app/src/App.js
+++ my-fast-app/src/App.js
@@ -7,54 +7,7 @@ import React,{useState } from 'react';
 
 
 function App()  {
-  //  const [name,setname]=useState('')
-  //  const [Email,setEmail]=useState('')
-  //  const [MobileNo,setMobileNo]=useState('')
-   
   
-  //   return(
-    
-    
-  //   <>
-  //     <form onSubmit={(e)=>{
-  //       e.preventDefault()
-  //       console.log(name,Email,MobileNo)
-  //     }}>
-  //     <h1>student form </h1>
-  //      <div>
-
-  //       <label>sname</label>
-  //       <br/>
-  //       <input type="text" value={name} 
-  //        onChange={(e)=>setname(e.target.value)}></input>
-  //      </div>
-  //      <div>
-  //       <label>Email</label>
-  //       <br/>
-  //       <input type="email" value={Email}
-  //        onChange={(e)=>{setEmail(e.target.value)}}></input>
-  //      </div>
-  //      <div>
-  //       <label>MobileNo</label>
-  //       <br/>
-  //       <input type="number" value={MobileNo} 
-  //        onChange={(e)=>{setMobileNo(e.target.value)}}></input>
-  //      </div>
-  //      <div>
-  //       <button>submit</button>
-  //      </div>
-  //      </form>
-  //      <div>
-  //       this is your details
-  //       <br/>
-  //       name:{name}
-  //       <br/>
-  //       Email:{Email}<br/>
-  //       MobileNo:{MobileNo}
-  //      </div>
-  //      <Button/>
-  //   </>
-  //  );
   return(
     <div>
       <h1>This my Today Assignment</h1>
